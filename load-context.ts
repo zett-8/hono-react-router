3110a85d29041f599b9a509bfd44559b65983c74
zett-8 <kame0008@gmail.com>
2026-04-26 17:55:16 +0200

    fix tsconfig

--- load-context.ts
+++ load-context.ts
@@ -1,5 +1,3 @@
-// eslint-disable-next-line @typescript-eslint/triple-slash-reference
-/// <reference path="./types/hono.d.ts" />
 import type { Context } from 'hono'
 import { type AppLoadContext } from 'react-router'
 import type { PlatformProxy } from 'wrangler'
